1db854667c31b70ab1566b7aeba9b5b82788223f
Jeff Wang <jeff.wang@infotrack.com.au>
2018-01-30 10:02:56 +1100

    ocr 2

---
+++ src/pages/OCR.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+// import * as apis from '../api';
+// import styles from '../styles/pages/ocr.less';
+
+export default class OCR extends React.Component {
+  render() {
+    return (
+      <div>
+        xxx
+      </div>
+    );
+  }
+}
